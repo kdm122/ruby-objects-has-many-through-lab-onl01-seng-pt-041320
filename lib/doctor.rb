3b0b732eb303825abccdef62d0eb2798b761f55e
Kyle McLean <kyledmclean@gmail.com>
2020-05-23 18:34:19 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,22 @@
+class Doctor
+  attr_accessor :name, :appointments
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def new_appointment(date, patient)
+    appt = Appointment.new(date, patient, self)
+    @appointments << appt
+  end
+
+
+end
